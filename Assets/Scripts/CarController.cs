bb179e226d0a759b3bbea0ab04a9cce94abd58f0
vianarya <vianaryasatya29@gmail.com>
2026-06-24 14:49:44 +0700

    update 24 juni

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -12,14 +12,40 @@ public class CarController : MonoBehaviour
     private Controller controls;
     private Vector2 moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = carData.CarSprite;
+        controls = new Controller();
+
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+        controls.CarControl.Movement.performed += movePlayer;
+        controls.CarControl.Movement.canceled += movePlayer;
+
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+        controls.CarControl.Movement.performed -= movePlayer;
+        controls.CarControl.Movement.canceled -= movePlayer;
+    }
+
+    public void movePlayer(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.MoveRotation(rb.rotation - moveInput.x * carData.TurnSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + (Vector2)transform.up * moveInput.y * carData.MoveSpeed * Time.fixedDeltaTime);
     }
 }
